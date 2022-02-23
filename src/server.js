dab0092afb003d4e2edfb625c2766fbe1934446a
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2022-02-22 22:21:59 -0300

    feat: initial server settings.

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/', (_request, response) => {
+  response.status(200).json({ message: 'Adriano Monteiro Dev' });
+});
+
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
